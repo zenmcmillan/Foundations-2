20648577b6aaf954d89271cc2c6832e0efcb94c7
Zen McMillan <zenmcmillan@gmail.com>
2023-07-31 18:06:09 -0600

    Changes

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,3 +1,28 @@
 module.exports = { 
-  //createFavoriteFood
+  createFavoriteFood,
+  commentOnSpiciness,
+  orderFood,
+  createShoppingList
 };
+
+function createFavoriteFood(dishObject) {
+  return {name: dishObject.dish, ingredients: dishObject.ingredients, isSpicy: dishObject.isSpicy, timesOrdered: 0}
+}
+
+function commentOnSpiciness(dishObject) {
+  if (dishObject.isSpicy) {
+    return `Wow, this ${dishObject.name} is so spicy!`
+  }
+  return `Phew, this ${dishObject.name} is not very spicy.`
+}
+
+function orderFood(dishObject) {
+  dishObject.timesOrdered = (dishObject.timesOrdered || 0) + 1
+  return dishObject
+}
+
+function createShoppingList(ingredientsArray) {
+ return ingredientsArray[0].ingredients.concat(ingredientsArray[1].ingredients)
+
+ 
+}
